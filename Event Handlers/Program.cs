b5442c7eaa6226235d675223bb1daef1da6b5361
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-08-13 11:47:15 +0100

    Event Handler

---
+++ Event Handlers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Event_Handlers
+{
+    class Program
+    {
+        class  SetTimeOut
+        {
+            int _snoozeInMilliSec = 1000;
+
+            public event EventHandler ValueChanged;
+
+            public void startNotification()
+            {
+                while (_snoozeInMilliSec > 0)
+                {
+                    Thread.Sleep(_snoozeInMilliSec);
+                    ValueChanged?.Invoke(this, new EventArgs());
+
+                    Console.WriteLine("Snooze for (in milli seconds, 0 to terminate): ");
+                    _snoozeInMilliSec = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            SetTimeOut obj = new SetTimeOut();
+            obj.ValueChanged += EventHandlerMethod;
+            obj.startNotification();
+            Console.ReadKey();
+        }
+        static void EventHandlerMethod(object sender, EventArgs e)
+        {
+            Console.WriteLine("EventHandlerMethod called at " + DateTime.Now.ToString("HH:mm:ss"));
+        }
+    }
+}
